c23a9aec89345d563926e6dcaee7727034787544
flo <{florian.sch10@gmail.com}>
2026-01-12 20:03:09 +0100

    feat: update theme and expand schema definitions

--- content.config.ts
+++ content.config.ts
@@ -1,10 +1,20 @@
-import { defineCollection, defineContentConfig } from '@nuxt/content'
+import { defineCollection, defineContentConfig, z } from '@nuxt/content'
 
 export default defineContentConfig({
   collections: {
     pages: defineCollection({
       type: 'page',
-      source: '**/*.md'
+      source: '*.md',
+      schema: z.object({
+        icon: z.string().optional(),
+      })
+    }),
+    hubPages: defineCollection({
+      type: 'page',
+      source: 'hub/*.md',
+      schema: z.object({
+        icon: z.string().optional(),
+      })
     })
   }
 })
