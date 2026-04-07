36e05089d3b2bcd2061430b3b43873ea29524f3f
flo <{florian.sch10@gmail.com}>
2026-04-07 14:00:12 +0200

    remove dotenv from env.ts for docker build

--- lib/env.ts
+++ lib/env.ts
@@ -1,6 +1,5 @@
 import { z } from 'zod';
 import tryParseEnv from './try-parse-env';
-import "dotenv/config";
 
 const envSchema = z.object({
     NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
